74e2a7d918678e6a498fb691c20955aa47cb9ac0
Abasiakan oyong <eddyenin6@gmail.com>
2022-08-17 10:53:05 +0100

    Initial commit

---
+++ resources/views/layouts/partials.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+     <!-- Styles -->
+     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+     <!-- Script -->
+     <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+    @yield('content')
+    
+</body>
+</html>
---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.partials')
+
+@section('content')
+    <div class="container">
+        <section class="profile">   
+            <div class="row">
+                <div class="col-md-12">
+                   
+                    <div class="profile-img">
+                        <img src="{{ asset('images/usr-profile.png')}}" alt="img" target="_blank">
+                    </div>
+                    <div class="profile-name">
+                        <h5>AKPAENIN EDIDIONG D.</h5>
+                        <p><i class="fa fa-envelope" aria-hidden="true"></i>   eddyenin6@gmail.com</p>
+                        <p><i class="fa fa-phone" aria-hidden="true"></i> +234-0703478726</p>
+                        <p><i class="fa fa-map-marker" aria-hidden="true"></i>   Lagos, Nigeria</p>
+                        <a href="" class="btn btn-md btn-secondary" download>Download Resume <i class="fa fa-download" aria-hidden="true"></i></a>
+                       
+                    </div>
+                    
+                </div>
+            </div>
+
+        </section>
+        <hr>
+
+        <section class="bio">
+            <p>An enthusiastic software developer with a positive and challenging mindset to achieve set goals. Experienced Software Engineer with a demonstrated history of working in the computer software industry. A well-seasoned software developer with a passion for developing innovative programs that help with the efficiency and effectiveness of organizational success.</p>
+        </section>
+        <hr>
+
+        <section class="social-media">
+            <a href="https://twitter.com/eddy_rexy" class="btn btn-md"><i class="fa fa-twitter-square"></i> Twitter</a>
+            <a href="https://linkedin.com/in/edidiong-daniel" class="btn btn-md"><i class="fa fa-linkedin"></i> LinkedIn</a>
+            <a href="https://github.com/eddyenin" class="btn btn-md"><i class="fa fa-github-square"></i> Github</a>
+        </section>
+        <hr>
+
+        <section class="skills">
+            <h4>Tech stack/skills</h4>
+            <div class="row">
+                <div class="col-md-6">
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>PHP</li>
+                        <li>Laravel</li>
+                        <li>Codeigniter</li>
+                    </ul>
+
+                </div>
+                <div class="col-md-6">
+                    <ul>
+                        <li>Git, Github</li>
+                        <li>MySQL</li>
+                        <li>Problem solving</li>
+                        <li>Excellent communication</li>
+                        <li>Team collaboration</li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </section>
+        <hr>
+        <section>
+            <div class="project-page">
+                <a href="" class="btn btn-md"><h5>PROJECTS <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></h5></a>
+            </div>
+        </section>
+        <hr>
+
+       
+    </div>
+@endsection
