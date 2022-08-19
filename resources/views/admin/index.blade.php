2dd23a8974dddad4beff66371cecd3fb2435e6c3
Abasiakan oyong <eddyenin6@gmail.com>
2022-08-19 03:20:07 +0100

    Project model

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.partials')
+
+@section('content')
+    <div class="container">
+        <nav class="navbar">
+            
+
+        </nav>
+        <span>Hello world!</span>
+    </div>
+    
+@endsection
